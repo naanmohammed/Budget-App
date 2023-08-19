fb8642040845550ec8a5097559de299c9965291b
naanmohammed <naanahmed24@gmail.com>
2023-08-19 09:11:40 +0300

    Update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :expenses, foreign_key: :author_id
   validates :name, presence: true
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  #:lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :validatable
 end
